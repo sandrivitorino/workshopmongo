baea98caed937228e267f35ddebecf55a8e577f8
vonvit <sandrineviturn@gmail.com>
2026-07-14 19:06:49 -0300

    project created

---
+++ src/test/java/com/sandri/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandri.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
