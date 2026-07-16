77a50cf12ef812c8ac03381e1b003a30a6468c50
vonvit <sandrineviturn@gmail.com>
2026-07-16 18:47:25 -0300

    Insercao de usuario com POST

--- src/main/java/com/sandri/workshopmongo/services/UserService.java
+++ src/main/java/com/sandri/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.sandri.workshopmongo.services;
 
 import com.sandri.workshopmongo.domain.User;
+import com.sandri.workshopmongo.dto.UserDTO;
 import com.sandri.workshopmongo.repository.UserRepository;
 import com.sandri.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
